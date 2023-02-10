08f83ce4651b9f87a8f6fd25ffdff3ad3a2e81dd
Максим Ермошин <maksim_ermoshin@mail.ru>
2023-02-11 00:54:48 +0300

    Starting to panic

--- yatube/core/models.py
+++ yatube/core/models.py
@@ -1,5 +1,27 @@
-from behaviors.behaviors import Timestamped
 from django.db import models
+from django.utils import timezone
+
+
+class Timestamped(models.Model):
+    """
+    An abstract behavior representing timestamping a model with``created`` and
+    ``modified`` fields.
+    """
+
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    modified = models.DateTimeField(null=True, blank=True, db_index=True)
+
+    class Meta:
+        abstract = True
+
+    @property
+    def changed(self):
+        return True if self.modified else False
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.modified = timezone.now()
+        return super(Timestamped, self).save(*args, **kwargs)
 
 
 class DefaultModel(models.Model):
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -126,5 +126,3 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     },
 }
-
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
